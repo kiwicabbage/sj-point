900b9703f6d8ac417573ac3e1148c0ad3b085a08
劉冠廷 <superstone@superstone.local>
2025-02-26 16:05:14 +0800

    Modify the Contract Symbol Name Error

--- src/sj_trading/getSymbol.py
+++ src/sj_trading/getSymbol.py
@@ -6,8 +6,6 @@ def selectContract(sj):
 
        all_contract = [i for i in (sj.api.Contracts.Options) if 
               str(i)[:2] == "TX"]
-
-
        # get begin symbol code
 
        BegCode = str(all_contract[0])[:3]
@@ -29,13 +27,20 @@ def selectContract(sj):
               contractList = all_contract[1]
        print(f'Nearest Contracts is {BegCode}')
 
-       now = datetime.datetime.now()
-       EndCodeC = (chr(now.month + 64) + str(now.year)[-1])
-       EndCodeP = (chr(now.month + 76) + str(now.year)[-1])
+       # now = datetime.datetime.now()
+       for i in contractList:
+              month = int(i.delivery_month[-2:])
+              year = i.delivery_month[:-2]
+
+              break
+       
+       EndCodeC = (chr(month + 64) + year[-1])
+       EndCodeP = (chr(month + 76) + year[-1])
        print(EndCodeC, EndCodeP)
 
        StrikePrices = (sorted([int(i.code[3:-2]) for i in contractList if i.code[-2:] == EndCodeP 
               and i.code[:3] == BegCode]))
+
 
        CallContracts = ([BegCode + str(i) + EndCodeC for i in StrikePrices])
        PutContracts = ([BegCode + str(i) + EndCodeP for i in StrikePrices])
@@ -46,3 +51,4 @@ def selectContract(sj):
 # snapshot return the buy price and the contract code
 
 
+# def getEndCode()
